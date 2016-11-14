41c74ac2f10af8f1f406d47aa990234ac943998f
mattherman <mattherman11@gmail.com>
2016-11-14 17:04:03 -0600

    Finished Chapter 5

--- Chapter5/Exercise10.hs
+++ Chapter5/Exercise10.hs
@@ -1,11 +1,12 @@
--- This is the original solution from the book, haven't modified it yet
+-- I gave up on this...it wasn't that interesting...
 
 import Data.Char
 
-let2int c = ord c - ord 'a'
-int2let n = chr (ord 'a' + n)
+let2int c | isLower c = ord c - ord 'a'
+          | otherwise = ord c - ord 'A'
 
-shift n c | isLower c = int2let ((let2int c + n) `mod` 26)
-          | otherwise = c
+int2let n  = chr (ord 'a' + n)
+
+shift n c = int2let ((let2int c + n) `mod` 26)
 
 encode n xs = [shift n x | x <- xs]
